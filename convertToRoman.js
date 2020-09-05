89f90b5c64d1a149cf695b2ac57b94b05d529248
gjgarot <garotg@gmail.com>
2020-09-05 11:55:50 -0700

    Add files via upload

--- convertToRoman.js
+++ convertToRoman.js
@@ -1,6 +1,6 @@
 var convertToRoman = function (num) {
-    var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    var roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
     numCount = num;
     num = "";
